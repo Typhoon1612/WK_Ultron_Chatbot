705e382f7551771b81e9ff1fdf8ee49d8afe6f76
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2023-05-05 16:11:51 +0800

    Add files via upload

--- Ultron/Ultron.py
+++ Ultron/Ultron.py
@@ -9,18 +9,15 @@ from nltk.corpus import wordnet
 # Define a list of possible responses to customer queries
 responses = {
     "greeting": ["Hello, how may I assist you?", "Welcome, how can I help you today?"],
-    "booking": ["You may proceed to FishAirline.com for booking!"],
+    "booking": ["You may proceed to UltronAirline.com for booking!"],
+    #notyet
     "support_channel": ["You can contact our support team at support@example.com or by calling 1-800-123-4567."],
     "available-country": ["\nOur flights only operate within Southeast Asian countries.\nWhich only included:\n1. Brunei\n2. Cambodia\n3. Indonesia\n4. Laos\n5. Malaysia\n6. Myanmar\n7. Philippines\n8. Singapore\n9. Thailand\n10. Timor-Leste\n11. Vietnam"],
-    "country_name": ["Fuck {country_name} people"],
-    "not_understand": ["Fuck you, humans, please type something that I can understand", "I'm sorry, I didn't understand your query. Please try again."],
+    "country_name": ["Yes, we have flight that are reaching {country_name}"],
+    "not_understand": ["Sorry, currently, we are only supporting Asean Country!!!", "I'm sorry, I didn't understand your query. Please try again."],
     "packages": ["2 Way Tickets", "1 Way Tickets"],
     "user_Intro": ["Hello {name}"],
-    "my_name": ["Your name is {name}"],
-    "task": ["Sure, let me assist you with that. Please provide your account information and I'll reset your password for you.",
-             "I can definitely help you with that. Please give me a moment to update your account information."],
-    "feedback": ["Thank you for your feedback. We'll take that into consideration as we continue to improve our services.", 
-                 "We appreciate your feedback and will use it to improve our products and services."]
+    "my_name": ["Your name is {name}"]
 }
 
 # Create a lemmatizer object
@@ -38,8 +35,10 @@ def lemmatize_sentence(sentence):
     lemmatized_sentence = " ".join(lemmatized_words)
     return lemmatized_sentence
 
-# Define a function to extract names from a sentence
-def extract_names(query):
+import nltk
+
+# Define a function to extract names and country names from a sentence
+def extract_names_and_countries(query):
     # Tokenize the sentence into words
     words = nltk.word_tokenize(query)
     # Tag the words with their part of speech
@@ -50,29 +49,17 @@ def extract_names(query):
     person_entities = [entity for entity in named_entities if isinstance(entity, nltk.tree.Tree) and entity.label() == "PERSON"]
     # Extract the person names from the person named entities
     person_names = [ " ".join([word[0] for word in entity.leaves()]) for entity in person_entities]
-    return person_names
-
-def extract_country_names(query):
-    # tokenize the text
-    tokens = nltk.word_tokenize(query)
-
-    # use Part-of-Speech (POS) tagging to tag the words with their part of speech
-    tagged_words = nltk.pos_tag(tokens)
-
-    # use Named Entity Recognition (NER) to label the named entities in the text
-    ner_tagged = nltk.ne_chunk(tagged_words)
-
-    # Filter out non-person named entities
-    country_entities = [entity for entity in ner_tagged if isinstance(entity, nltk.tree.Tree) and entity.label() == "GPE"]
-    # Extract the person names from the person named entities
+    # Filter out non-country named entities
+    country_entities = [entity for entity in named_entities if isinstance(entity, nltk.tree.Tree) and entity.label() == "GPE"]
+    # Extract the country names from the country named entities
     country_names = [ " ".join([word[0] for word in entity.leaves()]) for entity in country_entities]
-    return country_names
+    return person_names, country_names
 
 def booking_synonym(query):
     synonyms = []
     words = ["book", "buy"]
     for word in words:
-        for syn in wordnet.synsets(word, pos=['n', 'v']):
+        for syn in wordnet.synsets(word, pos='v'):
             for lemma in syn.lemmas():
                 synonyms.append(lemma.name())
     return synonyms
@@ -81,12 +68,9 @@ def booking_synonym(query):
 # Define a function to handle customer queries and generate responses
 def chatbot_response(query):
     global name  # Declare global variable to modify name inside the function
-
-    # Lemmatize the query
     query_lemmas = lemmatize_sentence(query.lower())
-    names = extract_names(query)
-    country_names = extract_country_names(query)
-    booking_synonym(query)
+    # Extract person names and country names from the query
+    names, country_names = extract_names_and_countries(query)
     if "hello" in query_lemmas or "hi" in query_lemmas and ("my" in query_lemmas and "name" in query_lemmas):
         # Check if the query includes the user's name
         if names:
@@ -99,42 +83,32 @@ def chatbot_response(query):
             return responses["my_name"][0].format(name=name)
         else:
             return "I'm sorry, I didn't catch your name. Can you please tell me your name?"
-    elif query in booking_synonym(query):
+    elif any(lemma in query_lemmas for lemma in booking_synonym(query)):
         return responses["booking"][0]
-        #elif "country" in query_lemmas:
-        #return responses["available-country"][0]
+    elif "where" in query_lemmas or ("which" in query_lemmas and "country"in query_lemmas):
+        return responses["available-country"][0]
     elif country_names:
         country_name = country_names[0]
         if country_name not in asean_countries:
-            return responses["not_understand"][0]
-            # Country is not in ASEAN
-            #return "I'm sorry, we currently only support countries in ASEAN."
+            return responses["not_understand"][0].format(country_name=country_name) 
         else:
             for country in asean_countries:
                 if country == country_name:
                     return responses["country_name"][0].format(country_name=country_name)
-    elif "support" in query_lemmas or "contact" in query_lemmas:
-        return random.choice(responses["support_channel"])
-    elif "reset" in query_lemmas or "update" in query_lemmas:
-        return random.choice(responses["task"])
-    elif "feedback" in query_lemmas or "review" in query_lemmas:
-        return random.choice(responses["feedback"])
     else:
         return responses["not_understand"][1]
-        #return "I'm sorry, I didn't understand your query. Please try again."
-
-
 
 # Define a function to handle user interaction
-'''
-def chatbot():
-    print("Welcome to our customer service chatbot!")
+'''def chatbot():
+    print("Welcome to Ultron Airline customer service chatbot!")
     while True:
         query = input("You: ")
+        #print(booking_synonym(query))
         if query.lower() == "exit":
             break
         response = chatbot_response(query)
         print("Chatbot: " + response)
+        print(booking_synonym(query))
         #print(person_names)
 
 # Run the chatbot function
